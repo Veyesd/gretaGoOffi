4c496a4575e31a992e151450d038664182e87be5
vixen <auriane.simonot@hotmail.com>
2020-11-05 16:15:35 +0100

    v

--- appIonic/src/app/pages/account/account.page.ts
+++ appIonic/src/app/pages/account/account.page.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
-
+  public form = [
+    { title: 'Formation : ', val: 'JS2020', isChecked: true },
+    { title: 'Anniversaire : ', val: '02 30 59 65 03', isChecked: false },
+    { title: 'Adresse : ', val: 'belee', isChecked: false },
+    { title: 'Email : ', val: 'belee@gejg/fr', isChecked: false },
+    { title: 'Permis : ', val: '', isChecked: false },
+  ];
+  showTrip = false;
   constructor() { }
 
   ngOnInit() {
-  }
+    this.showTrip = false;
 
+  }
+  show(){
+    this.showTrip = !this.showTrip;
+  }
+  getSelected(status: string){
+  } 
 }
